f6b4696820ccd7a5d56b54ec86b054e43c4e10c6
Madhu Kumari <madhu31082@gmail.com>
2025-04-29 15:59:11 +0530

    commit to add

--- coffee_shop_website/script.js
+++ coffee_shop_website/script.js
@@ -1,17 +1,29 @@
-let navbar = document.queryselector('.navbar');
+let navbar = document.querySelector('.navbar');
 
-document.queryselector('#menu-btn').onclick = () =>{
+document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
 }
 
-let searchForm = document.queryselector('.search-form');
+let searchForm = document.querySelector('.search-form');
 
-document.queryselector('#search-btn').onclick = () =>{
-    cartItem.classList.toggle('active');
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
 }
 
-let cartItem = document.queryselector('.cart-items-container');
+let cartItem = document.querySelector('.cart-items-container');
 
-document.queryselector('#cart-btn').onclick = () =>{
+document.querySelector('#cart-btn').onclick = () =>{
     cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
 }
